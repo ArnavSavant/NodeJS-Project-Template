f458de481b3b7eae1ec991a30a61a5a086247b1e
Arnav Savant <savantishu02@gmail.com>
2023-05-21 21:06:08 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const {serverConfig, Logger} = require('./config');
+const routes = require('./routes');
+
+const app = express();
+
+app.use('/api',routes);
+
+app.listen(serverConfig.PORT, () => {
+	console.log(`Succesfully listening on PORT: ${serverConfig.PORT}`);
+});
